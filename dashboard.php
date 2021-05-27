e16849e8f10e4dd5428f7c4810cb267e485ff7de
vjapp3 <56712854+vjapp3@users.noreply.github.com>
2021-05-27 10:16:54 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'source/session.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <?php if(!isset($_SESSION['username'])): header("location: logout.php");?>
+
+    <?php else: ?>
+
+    <?php endif ?>
+
+    <?php echo "<h1> <center>Welcome ".$_SESSION['username']." </center> </h1>" ?>
+
+    
+
+</body>
+</html>
